55d40eb2fa31c53d69bbfab9d0a4cb3b399fa749
cindered <asaas_96@hotmail.co.uk>
2023-02-23 12:46:37 +0000

    project descriptions

--- src/app/home/sections/projects/projects.component.ts
+++ src/app/home/sections/projects/projects.component.ts
@@ -11,7 +11,7 @@ export class ProjectsComponent {
     {
       name: 'Weather App',
       description:
-        'A simple weather web application search engine created using React.js and WeatherStack API.',
+        'A simple, reactive single page web application created using React.js and WeatherStack API to display the current weather anywhere in the world.',
       stack: ['React', 'React Spring', 'WeatherStack API'],
       repoLink: 'https://github.com/cindered/weather-app',
       liveLink: 'https://github.com/cindered/weather-app',
@@ -19,7 +19,7 @@ export class ProjectsComponent {
     {
       name: 'Chittr',
       description:
-        "A 'Twitter-Like' phone application created using React-Native which allows users to interact with others. Includes user profiles, logins, posts, geolocation, following and unfollowing features.",
+        "A 'Twitter-Like' phone application created using React-Native which uses communication with an API to handle data. Features included are user profiles, logins, posts, geolocation, following, unfollowing and more.",
       stack: ['React-Native', 'HTML', 'CSS', 'Node'],
       repoLink: 'https://github.com/cindered/chittr',
       liveLink: 'https://github.com/cindered/chittr',
@@ -27,7 +27,7 @@ export class ProjectsComponent {
     {
       name: 'Neural Network Package',
       description:
-        "An online interactive example that makes use of google's TensorFlow.js in an attempt to teach people about Deep Learning.",
+        "An online interactive example that makes use of google's TensorFlow.js in an attempt to teach people about Deep Learning with visual examples.",
       stack: ['JavaScript', 'HTML', 'CSS', 'Tensorflow'],
       repoLink: 'https://github.com/cindered/nnpackage',
       liveLink: 'https://cindered.github.io/nnpackage/',
